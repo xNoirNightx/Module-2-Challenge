52b6be5d7e5adb54b408a64b32e0b86e88ea4f0a
Coral <cnight18@hotmail.com>
2023-05-11 18:33:07 -0300

    trying to fix errors

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+/*======trying to add scrolling text for fun=====*/
+ScrollReveal({ 
+    reset: true,
+    duration: 1800,
+    distance: '80px',
+    delay: 150
+});
+
+ScrollReveal().reveal('.intro', '.title', { origin: 'top' });
